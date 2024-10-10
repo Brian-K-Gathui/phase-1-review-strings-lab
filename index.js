4b3c8711aec36c04fb7e568593c51400f00e2a1d
Brian <briankgathui@gmail.com>
2024-10-10 22:37:00 +0300

    Completing the Assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define currentUser
+const currentUser = 'Grace Hopper'; // Defining currentUser
+
+// Defining welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+
+// Defining excitedWelcomeMessage
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+
+// Defining shortGreeting
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; 
